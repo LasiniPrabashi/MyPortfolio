12785845e0dcc33554da070ba9f05f6a48bffab5
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2023-10-14 19:12:47 +0530

    update

---
+++ assignments/JS/SPA/db/db.js
@@ -0,0 +1,6 @@
+var customerDB = [
+    {id: "C00-001", name: "Lasini Prabashi", address: "Ahangama", salary: 4000},
+    {id: "C00-002", name: "Savindi Rashmika", address: "Matara", salary: 5000},
+    {id: "C00-003", name: "Nethmi Thathsarani", address: "Matara", salary: 300000},
+    {id: "C00-003", name: "Tharushi kavindya", address: "Panadura", salary: 300000}
+];
