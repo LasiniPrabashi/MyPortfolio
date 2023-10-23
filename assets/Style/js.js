210640a4d99b6358c91a8dc9f0d1ea51de2e43dd
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2023-10-23 15:55:16 +0530

    update

---
+++ assets/Style/js.js
@@ -0,0 +1,36 @@
+const text = document.querySelector(".h5");
+const textLoad = () => {
+    setTimeout(() =>{
+        text.textContent = "Front-End Developer";
+    },0);
+    setTimeout(() =>{
+        text.textContent = "Graphic Designer";
+    },4000);
+}
+textLoad();
+setInterval(textLoad,8000);
+
+
+/scroll section/
+let sections = document.querySelectorAll('section');
+let section = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    /sticky header/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>100);
+
+    /remove icon and navbar when click navbar/
+    menuIcon.classList.remove(' fa-xmark');
+    navbar.classList.remove('active');
+}
+
+
+/menu icon/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar =document.querySelector('.nav_bar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
